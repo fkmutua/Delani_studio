855fadc76a2e9ccb97e03a9e880b20a29f436f6a
felix kasyoki mutua <fkmutua@safaricom.co.ke>
2020-06-30 06:03:57 -0700

    js modified to effect zoom effect and overlay effects

--- js/script.js
+++ js/script.js
@@ -15,16 +15,16 @@ $(document).ready(function() {
 
 $(document).ready(function() {
     $("div.image-frame").hover(function() {
-        $(".classAdd1").addClass("mask flex-center rgba-red-strong");
+        $(".strong").addClass("mask flex-center rgba-red-strong");
     });
     $("div.image-frame").hover(function() {
-        $(".classAdd2").addClass("mask flex-center rgba-blue-light");
+        $(".light").addClass("mask flex-center rgba-blue-light");
     });
     $("div.image-frame").hover(function() {
-        $(".classAdd3").addClass("mask flex-center rgba-green-slight");
+        $(".super").addClass("mask flex-center rgba-green-slight");
     });
     $("div.image-frame").hover(function() {
-        $(".classAdd4").addClass("mask flex-center");
+        $(".zoom").addClass("mask flex-center");
     });
 });
 
