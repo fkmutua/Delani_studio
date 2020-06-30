39acc5a17b3a4ed1e2f58717ddad9936b7e2aabe
felix kasyoki mutua <fkmutua@safaricom.co.ke>
2020-06-30 05:20:16 -0700

    Added messaging part for the response when a person contacts us

--- js/script.js
+++ js/script.js
@@ -33,8 +33,7 @@ function myMessage() {
     var email = document.getElementById("email").value;
     var message = document.getElementById("comment").value;
     if (email !== "" && name !== "" && message !== "") {
-        // document.getElementById("pop").innerHTML =
-        alert("Hello" + " " + name + " we have received your message. Thank you for reaching out to us.");
+        alert("Thanks " + " " + name + " we have received your message. Thank you for reaching out to us.");
     } else if (email == "" || name !== "" || message !== "") {
         alert("Please fill in all the details");
     }
